80363d73792aace71efbcdddeb76ab0670993746
blash4u <polygoncity@gmail.com>
2026-09-17 15:44:00 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbyuq4W5XUIfzE3ItpWYnADBTMz4d6PVo",
+  authDomain: "jw-biblequiz-app.firebaseapp.com",
+  projectId: "jw-biblequiz-app",
+  storageBucket: "jw-biblequiz-app.firebasestorage.app",
+  messagingSenderId: "656796223053",
+  appId: "1:656796223053:web:df0d5db40ea40461453e04"
+};
