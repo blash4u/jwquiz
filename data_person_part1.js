f7b722c94632a1cfeb11ca7dd70d705eac2d8cf4
blash4u <polygoncity@gmail.com>
2026-09-22 15:33:03 +0900

    Update data_person_part1.js

--- data_person_part1.js
+++ data_person_part1.js
@@ -1,12 +1,13 @@
 // data_person_part1.js
 // 「용기 있게 하느님과 함께 걸으십시오」 1부 (1장~21장) 전체 인물 데이터
+// 공식 CDN 이미지 URL 규칙 매칭 완료 (1102025901 ~ 1102025921)
 
 export const personPart1 = [
-  // 1장: 에녹
+  // 1장: 에녹 (01)
   {
     id: "person_ch01",
     chapter: 1,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025901/univ/art/1102025901_univ_cnt_1_xl.jpg",
     clues: [
       "아담의 7대손으로, 극도로 불경건하고 폭력적인 세상에서 참하느님과 함께 계속 걸었습니다.",
       "모든 불경건한 자들에게 여호와의 심판이 집행될 것임을 과거 시제를 사용하여 확실하게 선언했습니다.",
@@ -17,11 +18,11 @@ export const personPart1 = [
     reference: "창세기 5:21-24; 유다서 14, 15"
   },
 
-  // 2장: 노아
+  // 2장: 노아 (02)
   {
     id: "person_ch02",
     chapter: 2,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025902/univ/art/1102025902_univ_cnt_1_xl.jpg",
     clues: [
       "타락한 천사들과 포악한 네피림이 가득한 세상에서 참하느님과 함께 걸은 의인이었습니다.",
       "거대한 방주를 약 50년 동안 만들면서 사람들에게 다가올 대홍수를 알리는 '의의 전파자'로 일했습니다.",
@@ -32,11 +33,11 @@ export const personPart1 = [
     reference: "창세기 6:9-22; 베드로 후서 2:5"
   },
 
-  // 3장: 사라
+  // 3장: 사라 (03)
   {
     id: "person_ch03",
     chapter: 3,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025903/univ/art/1102025903_univ_cnt_1_xl.jpg",
     clues: [
       "편안하고 안전한 도시 우르를 뒤돌아보지 않고 기꺼이 남편을 따라 낯선 천막생활을 시작했습니다.",
       "이집트 궁전으로 불려가는 위협 속에서도 하느님의 도우심으로 충절을 굳게 지켜 냈습니다.",
@@ -47,11 +48,11 @@ export const personPart1 = [
     reference: "창세기 12:1-20; 17:15-19; 히브리서 11:11"
   },
 
-  // 4장: 아브라함 (첫 번째 활약: 의로운 전쟁)
+  // 4장: 아브라함 - 의로운 전쟁 (04)
   {
     id: "person_ch04",
     chapter: 4,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025904/univ/art/1102025904_univ_cnt_1_xl.jpg",
     clues: [
       "포로로 잡혀간 조카 롯을 구출하기 위해 집안의 훈련된 사병 318명을 이끌고 북쪽으로 추격했습니다.",
       "밤에 기습 공격을 감행하여 강력한 네 왕의 연합군을 다마스쿠스 근처까지 물리쳤습니다.",
@@ -62,11 +63,11 @@ export const personPart1 = [
     reference: "창세기 14:1-24; 히브리서 7:1-4"
   },
 
-  // 5장: 아브라함 (두 번째 활약: 모리아산 시험)
+  // 5장: 아브라함 - 모리아산 순종 (05)
   {
     id: "person_ch05",
     chapter: 5,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025905/univ/art/1102025905_univ_cnt_1_xl.jpg",
     clues: [
       "자신이 100세에 얻은 가장 사랑하는 독자 이삭을 번제물로 바치라는 극심한 시험을 받았습니다.",
       "하느님께서 능히 아들을 부활시키실 수 있다는 확고한 믿음으로 모리아산으로 3일 길을 걸어갔습니다.",
@@ -77,11 +78,11 @@ export const personPart1 = [
     reference: "창세기 22:1-19; 히브리서 11:17-19"
   },
 
-  // 6장: 리브가
+  // 6장: 리브가 (06)
   {
     id: "person_ch06",
     chapter: 6,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025906/univ/art/1102025906_univ_cnt_1_xl.jpg",
     clues: [
       "우물가에서 아브라함의 종과 그의 낙타 열 마리에게 헌신적으로 물을 길어 주었습니다.",
       "가족과 고향을 떠나 한 번도 본 적 없는 이삭과의 결혼을 위해 '기꺼이 가겠습니다'라고 대답했습니다.",
@@ -92,26 +93,26 @@ export const personPart1 = [
     reference: "창세기 24:1-67; 25:20"
   },
 
-  // 7장: 야곱
+  // 7장: 야곱 (07)
   {
     id: "person_ch07",
     chapter: 7,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025907/univ/art/1102025907_univ_cnt_1_xl.jpg",
     clues: [
       "라반 밑에서 20년간 불공정한 대우를 받으면서도 인내하며 가족을 지켰습니다.",
       "하느님의 축복을 얻기 위해 밤새도록 천사와 끈질기게 씨름하여 '이스라엘'이라는 이름을 얻었습니다.",
-      "부하 400명을 이끌고 오는 형 에서 앞에 나아가 일곱 번 몸을 굽힘으로 화해와 평화를 이루었습니다."
+      "부하 400명을 이끌고 다가오는 형 에서 앞에 나아가 일곱 번 몸을 굽힘으로 화해와 평화를 이루었습니다."
     ],
     correctAnswer: "야곱",
     distractors: ["에서", "요셉", "라반"],
     reference: "창세기 31:36-55; 32:1-33:16"
   },
 
-  // 8장: 요셉
+  // 8장: 요셉 (08)
   {
     id: "person_ch08",
     chapter: 8,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025908/univ/art/1102025908_univ_cnt_1_xl.jpg",
     clues: [
       "형들의 질투로 구덩이에 던져진 뒤 이집트의 종으로 팔려 가는 비극을 겪었습니다.",
       "보디발 아내의 날마다 계속된 성적 유혹을 '내가 어찌 하느님께 죄를 짓겠습니까'라며 단호히 뿌리쳤습니다.",
@@ -122,11 +123,11 @@ export const personPart1 = [
     reference: "창세기 39:1-23; 41:1-45:28"
   },
 
-  // 9장: 십브라와 부아, 아므람과 요게벳, 미리암
+  // 9장: 십브라, 부아, 아므람, 요게벳, 미리암 (09)
   {
     id: "person_ch09",
     chapter: 9,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025909/univ/art/1102025909_univ_cnt_1_xl.jpg",
     clues: [
       "파라오의 유아 살해 명령을 두려워하지 않고 사내아이들을 목숨 걸고 살려 준 히브리 산파들입니다.",
       "갓 태어난 아기를 3개월간 숨기다가 파피루스 바구니에 담아 나일강 갈대 사이에 뉘어 지켜 낸 부모입니다.",
@@ -137,11 +138,11 @@ export const personPart1 = [
     reference: "출애굽기 1:15-2:10; 히브리서 11:23"
   },
 
-  // 10장: 모세 (첫 번째 활약: 올바른 선택)
+  // 10장: 모세 - 올바른 선택 (10)
   {
     id: "person_ch10",
     chapter: 10,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025910/univ/art/1102025910_univ_cnt_1_xl.jpg",
     clues: [
       "이집트 궁중의 왕자로서 모든 지혜를 배웠으나 파라오의 딸의 아들이라 불리기를 거절했습니다.",
       "잠시 죄의 낙을 누리기보다 하느님의 백성과 함께 학대받는 길을 선택했습니다.",
@@ -152,11 +153,11 @@ export const personPart1 = [
     reference: "출애굽기 2:11-22; 히브리서 11:24-26"
   },
 
-  // 11장: 모세 (두 번째 활약: 파라오 앞에서의 용기)
+  // 11장: 모세 - 파라오 앞에서의 용기 (11)
   {
     id: "person_ch11",
     chapter: 11,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025911/univ/art/1102025911_univ_cnt_1_xl.jpg",
     clues: [
       "호렙산 불타는 가시덤불 가운데서 하느님의 부르심을 받고 이집트로 돌아갔습니다.",
       "분노한 파라오 앞에 거듭 나아가 열 가지 재앙의 심판을 선언했습니다.",
@@ -167,11 +168,11 @@ export const personPart1 = [
     reference: "출애굽기 7:1-14:31; 신명기 34:5-7"
   },
 
-  // 12장: 갈렙
+  // 12장: 갈렙 (12)
   {
     id: "person_ch12",
     chapter: 12,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025912/univ/art/1102025912_univ_cnt_1_xl.jpg",
     clues: [
       "10명의 정탐꾼이 악평을 퍼뜨려 온 백성이 공포에 질렸을 때 '여호와께서 우리와 함께하신다'고 외쳤습니다.",
       "백성들이 돌로 치려 하는 위험 속에서도 하느님을 향한 충성과 믿음을 굽히지 않았습니다.",
@@ -182,11 +183,11 @@ export const personPart1 = [
     reference: "민수기 13:1-14:38; 여호수아 14:6-15"
   },
 
-  // 13장: 여호수아
+  // 13장: 여호수아 (13)
   {
     id: "person_ch13",
     chapter: 13,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025913/univ/art/1102025913_univ_cnt_1_xl.jpg",
     clues: [
       "모세의 뒤를 이어 이스라엘 백성을 이끌고 요르단강을 건너 약속의 땅으로 들어갔습니다.",
       "이해하기 어려운 군사 명령이었지만 예리코 성벽 주위를 7일간 행진하라는 지시에 철저히 순종했습니다.",
@@ -197,11 +198,11 @@ export const personPart1 = [
     reference: "여호수아 1:1-9; 6:1-21; 24:15"
   },
 
-  // 14장: 라합
+  // 14장: 라합 (14)
   {
     id: "person_ch14",
     chapter: 14,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025914/univ/art/1102025914_univ_cnt_1_xl.jpg",
     clues: [
       "예리코 성벽 위에 살던 여인이었지만 여호와께서 행하신 위대한 구원의 소식을 듣고 믿음을 가졌습니다.",
       "이스라엘의 정탐꾼 두 명을 지붕 위 아마 줄기 사이에 숨겨 주고 탈출하도록 도와주었습니다.",
@@ -212,11 +213,11 @@ export const personPart1 = [
     reference: "여호수아 2:1-24; 6:14-25; 히브리서 11:31"
   },
 
-  // 15장: 나오미와 룻
+  // 15장: 나오미와 룻 (15)
   {
     id: "person_ch15",
     chapter: 15,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025915/univ/art/1102025915_univ_cnt_1_xl.jpg",
     clues: [
       "남편과 두 아들을 모두 잃은 극심한 비극 속에서도 서로를 깊이 위로하고 지탱해 준 고부입니다.",
       "'어머니의 백성이 저의 백성이 되고 어머니의 하느님이 저의 하느님이 되실 것입니다'라는 서약을 남겼습니다.",
@@ -227,11 +228,11 @@ export const personPart1 = [
     reference: "룻기 1:1-4:22"
   },
 
-  // 16장: 바락과 드보라
+  // 16장: 바락과 드보라 (16)
   {
     id: "person_ch16",
     chapter: 16,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025916/univ/art/1102025916_univ_cnt_1_xl.jpg",
     clues: [
       "여예언자의 조언을 존중하여 1만 명의 군사를 모아 다볼산으로 올라간 이스라엘의 재판관입니다.",
       "철낫 달린 병거 900대로 무장한 가나안 장군 시스라의 대군과 맞서 용감히 싸웠습니다.",
@@ -242,11 +243,11 @@ export const personPart1 = [
     reference: "사사기 4:1-16; 5:1-23"
   },
 
-  // 17장: 야엘
+  // 17장: 야엘 (17)
   {
     id: "person_ch17",
     chapter: 17,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025917/univ/art/1102025917_univ_cnt_1_xl.jpg",
     clues: [
       "패주하여 도망쳐 온 잔인한 군대 대장 시스라에게 담요를 덮어 주고 젖을 주어 안심시켰습니다.",
       "적장이 깊이 잠들었을 때 망치와 천막 말뚝을 사용하여 그를 처단했습니다.",
@@ -257,11 +258,11 @@ export const personPart1 = [
     reference: "사사기 4:17-22; 5:24-31"
   },
 
-  // 18장: 기드온
+  // 18장: 기드온 (18)
   {
     id: "person_ch18",
     chapter: 18,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025918/univ/art/1102025918_univ_cnt_1_xl.jpg",
     clues: [
       "미디안의 약탈을 피해 포도주틀 속에서 밀을 타작하던 조심성 있는 사람이었습니다.",
       "양털 뭉치에 내린 이슬 시험을 통해 하느님이 함께하신다는 확신을 얻었습니다.",
@@ -272,11 +273,11 @@ export const personPart1 = [
     reference: "사사기 6:1-8:28"
   },
 
-  // 19장: 입다와 그의 딸
+  // 19장: 입다와 그의 딸 (19)
   {
     id: "person_ch19",
     chapter: 19,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025919/univ/art/1102025919_univ_cnt_1_xl.jpg",
     clues: [
       "암몬 사람들과의 전쟁에서 승리하면 집 문에서 가장 먼저 나오는 자를 여호와께 바치겠다고 서원했습니다.",
       "전쟁 후 승리하고 돌아왔을 때 탬버린을 치며 가장 먼저 마중 나온 외동딸을 보고 옷을 찢었습니다.",
@@ -287,11 +288,11 @@ export const personPart1 = [
     reference: "사사기 11:1-40"
   },
 
-  // 20장: 삼손
+  // 20장: 삼손 (20)
   {
     id: "person_ch20",
     chapter: 20,
-    image: "https://assets.jw.org/assets/m/wcg/univ/wcg_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025920/univ/art/1102025920_univ_cnt_1_xl.jpg",
     clues: [
       "모태에서부터 나실인으로 구별되어 머리카락을 깎지 않고 초인적인 힘으로 블레셋과 싸웠습니다.",
       "가자의 성문 문짝과 기둥을 뜯어 어깨에 메고 헤브론 산꼭대기까지 걸어 올라갔습니다.",
@@ -302,11 +303,11 @@ export const personPart1 = [
     reference: "사사기 13:1-16:31; 히브리서 11:32"
   },
 
-  // 21장: 사무엘
+  // 21장: 사무엘 (21)
   {
     id: "person_ch21",
     chapter: 21,
-    image: "https://assets.jw.org/assets/m/ia/univ/ia_univ_lsr_xl.jpg",
+    image: "https://cms-imgp.jw-cdn.org/img/p/1102025921/univ/art/1102025921_univ_cnt_1_xl.jpg",
     clues: [
       "어머니 한나의 기도로 태어나 젖을 떼자마자 실로의 장막에 바쳐져 자랐습니다.",
       "엘리의 아들들이 타락했을 때에도 물들지 않고 밤중에 들려온 여호와의 부르심에 귀를 기울였습니다.",
